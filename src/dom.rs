cc00c883b6908b2c0b31af958a297c9cf49b481a
Jonathan Gil <jhongilp@gmail.com>
2020-07-06 22:04:31 -0500

    initial dom structure

---
+++ src/dom.rs
@@ -0,0 +1,30 @@
+struct Node {
+    children: Vec<Node>,
+    node_type: NodeType,
+}
+
+enum NodeType {
+    Text(String),
+    Element(ElementData),
+}
+
+struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
+}
+
+type AttrMap = HashMap<String, String>;
+
+fn text(data: String) -> Node {
+    Node { children: Vec::new(), node_type: NodeType::Text(data)}
+}
+
+fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+    Node {
+        children,
+        node_type: NodeType::Element( ElementData {
+            tag_name: name,
+            attributes: attrs,
+        })
+    }
+}
