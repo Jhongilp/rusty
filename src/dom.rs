2e22c479a81368c960ec5176aed0c58b8304bdbf
Jonathan Gil <jhongilp@gmail.com>
2020-07-08 22:18:18 -0500

    add consume_while impl

--- src/dom.rs
+++ src/dom.rs
@@ -16,19 +16,21 @@ struct ElementData {
 type AttrMap = HashMap<String, String>;
 
 fn text(data: String) -> Node {
-    Node { children: Vec::new(), node_type: NodeType::Text(data)}
+    Node {
+        children: Vec::new(),
+        node_type: NodeType::Text(data),
+    }
 }
 
 fn elem(name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
     Node {
         children,
-        node_type: NodeType::Element( ElementData {
+        node_type: NodeType::Element(ElementData {
             tag_name: name,
             attributes: attrs,
-        })
+        }),
     }
 }
-
 
 // PARSER ++++++++++++++
 
@@ -45,7 +47,7 @@ impl Parser {
 
     // do the next characters start with the given string?
     fn starts_with(&self, s: &str) -> bool {
-        self.input[self.pos ..].starts_with(s)
+        self.input[self.pos..].starts_with(s)
     }
 
     // return true if all input is consumed
@@ -60,5 +62,17 @@ impl Parser {
         let (next_pos, _) = iter.next().unwrap_or((1, ' '));
         self.pos += next_pos;
         return cur_char;
+    }
+
+    // consume character until 'test' returns false
+    fn consume_while<F>(&mut self, test: F) -> String
+    where
+        F: Fn(char) -> bool,
+    {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        return result;
     }
 }
